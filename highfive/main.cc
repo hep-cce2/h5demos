af195f293823471976916effc4ba9a94ca1e6ad7
Saba Sehrish <ssehrish@fnal.gov>
2021-05-19 13:12:48 -0500

    initial version of code from h5demos repository

---
+++ highfive/main.cc
@@ -0,0 +1,17 @@
+#include "h5_write_chars.h"
+
+int main(int argc, char* argv[]) {
+  if (argc != 4) {
+    std::cerr << "Please specify input file, output file and batch size\n";
+    return 1;
+  }
+  auto input_root_fname = argv[1];
+  auto output_h5_fname = argv[2];
+  int batch_size = atoi(argv[3]);
+  auto run_num = 1;
+  auto lumi_num = 1;
+  //batch_size is currently one, writing one element at a time!!!
+  h5_write_ds(input_root_fname, 1, run_num, lumi_num, output_h5_fname);
+  return 0;
+}
+
